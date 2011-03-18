4a21b9da42daf27064f2ad4caca07730ef699a34
Leszek <andrukanisleszek@gmail.com>
2011-03-18 17:31:05 +0100

    app cleaning

--- db/20110111161945_add_uniqueness_on_email.rb
+++
@@ -1,9 +0,0 @@
-class AddUniquenessOnEmail < ActiveRecord::Migration
-  def self.up
-    add_index :users, :email, :unique => true
-  end
-
-  def self.down
-    add_index :users, :email
-  end
-end
